ae019be4eba7a8cb43f75ba9e0c99db54b44297c
Eric Rodewald <eric.rode@gmail.com>
2019-03-10 17:31:58 -0500

    Test: Fix tests for new default setting

--- test/smartapp-page-spec.js
+++ test/smartapp-page-spec.js
@@ -10,6 +10,7 @@ describe('smartapp-page-spec', () => {
 			page.section('whenDoorOpensAndCloses', section => {
 				section.deviceSetting('contactSensor')
 					.capabilities(['contactSensor'])
+					.required(true)
 			})
 
 			page.section('turnLightsOnAndOff', section => {
@@ -17,6 +18,7 @@ describe('smartapp-page-spec', () => {
 					.capabilities(['switch'])
 					.multiple(true)
 					.permissions('rx')
+					.required(true)
 			})
 		})
 
