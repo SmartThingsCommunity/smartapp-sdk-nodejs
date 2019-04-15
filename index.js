d7a52b19f45c6b0519f2db702e003a18aa95b3c5
Eric Rodewald <eric.rode@gmail.com>
2019-04-15 09:24:33 -0500

    docs: add jsdocs
    fix: dep vulns
    
    chore: add start and debug scripts
    
    docs: add typedefs to api
    
    
    docs: add typedef to endpoint-context api
    
    
    docs: add util/events.js for typedefs
    
    
    docs: add jsdocs to signature.js
    
    
    feat: add ColorSetting, MessageGroupSetting w/ jsdocs
    
    
    docs: add jsdocs to all *settings
    
    
    docs: add jsdocs to smart-app.js
    
    
    Remove index.js
    
    
    chore: remove comments from events.js
    
    docs: add mode, device, and timer event docs
    
    
    split string by regex properly
    
    
    use correct return result
    
    
    remove events.js
    
    
    set this.api = {} for rules
    
    
    Add .vscode launch.json to debug mocha tests easily
    
    
    Use proper jsdoc type over typedef
    
    
    docs: update readme
    
    
    chore: add boolean-setting-spec
    
    
    docs: add SmartAppOptions object
    
    
    fix usage of typedef vs type in api.js

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const SmartApp = require('./lib/smart-app')
-
-module.exports = (function (options = {}) {
-	return new SmartApp(options)
-})()
-
-module.exports.default = Object.assign({}, module.exports)
