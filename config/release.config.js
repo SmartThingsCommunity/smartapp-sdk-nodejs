752d6e4c2472a414700f23ccccede173d21a077c
Eric Rodewald <eric.rode@gmail.com>
2019-05-06 23:35:09 -0500

    ci: Call npm script semantic-release
    Add semantic-release config
    
    
    ci: add/update dependencies 
    
    
    Update package-lock
    
    
    Add semantic-release script
    
    
    Add xo rule for no-template-curly-in-string

--- config/release.config.js
+++ config/release.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-	analyzeCommits: {
-		preset: 'eslint'
-	},
-	generateNotes: {
-		preset: 'eslint'
-	}
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		['@semantic-release/changelog', {
+			changelogFile: 'docs/CHANGELOG.md'
+		}],
+		'@semantic-release/npm',
+		['@semantic-release/git', {
+			assets: ['docs', 'package.json'],
+			message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+		}]
+	]
 }
