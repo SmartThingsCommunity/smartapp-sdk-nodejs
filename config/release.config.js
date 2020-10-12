6df10a2cdeae280556548904cd0d7a9b8edeaccc
John Undersander <john.undersander@smartthings.com>
2020-10-12 11:14:21 -0500

    ci: enable github releases (#168)

--- config/release.config.js
+++ config/release.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
+		'@semantic-release/github',
 		['@semantic-release/changelog', {
 			changelogFile: 'docs/CHANGELOG.md'
 		}],
