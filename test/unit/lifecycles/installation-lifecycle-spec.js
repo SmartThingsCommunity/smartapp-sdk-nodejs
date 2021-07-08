6cb71accb21f428b528986d17bb86ad7fce9745d
John Undersander <john.undersander@smartthings.com>
2021-07-08 10:13:41 -0500

    fix: await async updated handlers during install
    * fix #200

--- test/unit/lifecycles/installation-lifecycle-spec.js
+++ test/unit/lifecycles/installation-lifecycle-spec.js
@@ -1,94 +1,83 @@
-const SmartApp = require('../../../lib/smart-app')
+const { SmartApp } = require('../../../lib/smart-app')
+const { INSTALL, UNINSTALL, UPDATE } = require('../../data/lifecycles')
 
 describe('installation-lifecycle-spec', () => {
 	let app
-	let expectedData
 	let receivedEvent
 	let receivedData
 
 	beforeEach(() => {
-		app = new SmartApp({logUnhandledRejections: false})
-		expectedData = {
-			authToken: 'string',
-			refreshToken: 'string',
-			installedApp: {
-				installedAppId: 'd692699d-e7a6-400d-a0b7-d5be96e7a564',
-				locationId: 'e675a3d9-2499-406c-86dc-8a492a886494',
-				config: {}
-			}
-		}
+		app = new SmartApp({ logUnhandledRejections: false })
+		receivedEvent = undefined
+		receivedData = undefined
 	})
 
-	it('should handle INSTALL lifecycle', async () => {
+	it('handles INSTALL lifecycle with installed handler', async () => {
 		app.installed((_, installData) => {
 			receivedData = installData
 		})
-		await app.handleMockCallback({
-			lifecycle: 'INSTALL',
-			executionId: 'e6903fe6-f88f-da69-4c12-e2802606ccbc',
-			locale: 'en',
-			version: '0.1.0',
-			client: {
-				os: 'ios',
-				version: '0.0.0',
-				language: 'en-US'
-			},
-			installData: expectedData,
-			settings: {}
-		})
 
-		expect(receivedData).toStrictEqual(expectedData)
+		await app.handleMockCallback(INSTALL)
+
+		expect(receivedData).toStrictEqual(INSTALL.installData)
 	})
 
-	it('should handle UNINSTALL lifecycle', async () => {
-		const expectedEvent = {
-			'installedApp': {
-				'installedAppId': 'd46a1c60-b6bd-4f82-b124-028e0f14a4f4',
-				'locationId': 'e1e66eab-1eab-4f09-9bb6-91da6585576d',
-				'config': {},
-				'permissions': []
-			}
-		}
+	it('handles INSTALL lifecycle with updated handler when no installed handler defined', async () => {
+		app.updated((_, installData) => {
+			receivedData = installData
+		})
+
+		await app.handleMockCallback(INSTALL)
+
+		expect(receivedData).toStrictEqual(INSTALL.installData)
+	})
+
+	it('handles INSTALL lifecycle rejection with async updated handler when no installed handler defined', async () => {
+		app.updated(async () => {
+			await Promise.reject(new Error('something failed'))
+		})
+
+		const response = app.handleMockCallback(INSTALL)
+
+		await expect(response).resolves.toStrictEqual(
+			expect.objectContaining({ statusCode: 500, message: expect.stringContaining('something failed') })
+		)
+	})
+
+	it('handles INSTALL lifecycle with installed handler when both installed and updated handlers defined', async () => {
+		app.installed((_, installData) => {
+			receivedData = installData
+		})
+
+		const updateHandler = jest.fn(() => {
+			receivedData = null
+		})
+
+		app.updated(updateHandler)
+
+		await app.handleMockCallback(INSTALL)
+
+		expect(updateHandler).not.toBeCalled()
+		expect(receivedData).toStrictEqual(INSTALL.installData)
+	})
+
+	it('handles UNINSTALL lifecycle', async () => {
 		app.uninstalled((_, event) => {
 			receivedEvent = event
 		})
-		await app.handleMockCallback({
-			'lifecycle': 'UNINSTALL',
-			'executionId': 'e139dc1f-ee24-3c1a-309e-48669006817f',
-			'locale': 'en-US',
-			'version': '0.1.0',
-			'uninstallData': {
-				'installedApp': {
-					'installedAppId': 'd46a1c60-b6bd-4f82-b124-028e0f14a4f4',
-					'locationId': 'e1e66eab-1eab-4f09-9bb6-91da6585576d',
-					'config': {},
-					'permissions': []
-				}
-			},
-			'settings': {}
-		})
 
-		expect(receivedEvent).toStrictEqual(expectedEvent)
+		await app.handleMockCallback(UNINSTALL)
+
+		expect(receivedEvent).toStrictEqual(UNINSTALL.uninstallData)
 	})
 
-	it('should handle UPDATE lifecycle', async () => {
+	it('handles UPDATE lifecycle', async () => {
 		app.updated((_, updateData) => {
 			receivedData = updateData
 		})
-		await app.handleMockCallback({
-			lifecycle: 'UPDATE',
-			executionId: 'e6903fe6-f88f-da69-4c12-e2802606ccbc',
-			locale: 'en',
-			version: '0.1.0',
-			client: {
-				os: 'ios',
-				version: '0.0.0',
-				language: 'en-US'
-			},
-			updateData: expectedData,
-			settings: {}
-		})
 
-		expect(receivedData).toStrictEqual(expectedData)
+		await app.handleMockCallback(UPDATE)
+
+		expect(receivedData).toStrictEqual(UPDATE.updateData)
 	})
 })
