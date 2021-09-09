e2e2d6f4c3a3a223003f00265220b42443d3cb93
Bob Florian <bob@smartthings.com>
2021-09-09 14:15:25 -0400

    feat: Exported Page class to support unit testing of SmartApps

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 const SmartApp = require('./lib/smart-app')
+const Page = require('./lib/pages/page')
 
 module.exports = SmartApp
 module.exports.SmartApp = SmartApp
+module.exports.Page = Page
